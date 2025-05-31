22421e33789e596c6569380f9a3fb9441bd6cdc2
Mehak Khanna <mehakkhanna2218@gmail.com>
2025-05-31 15:14:31 +0530

    Landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@/components/Card";
 import Footer from "@/components/Footer";
+import GridItem from "@/components/GridItem";
 import LottieAnimation from "@/components/LottieAnimation";
 import Navbar from "@/components/Navbar"; // adjust path if needed
 import { ProcessStep } from "@/components/ProcessStep";
@@ -360,49 +361,3 @@ export default function Home() {
   );
 }
 
-// mt-12 lg:mt-0 lg:ml-10 flex justify-center items-center
-
-{
-  /* <nav className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-qubix-pink">QUBIX</h1>
-        <button className="bg-qubix-pink text-white px-4 py-2 rounded-md hover:bg-pink-600">
-          Book a Demo
-        </button>
-      </nav> */
-}
-
-interface GridItemProps {
-  icon: React.ReactNode;
-  title: string;
-  description: React.ReactNode;
-}
-const GridItem = ({ icon, title, description }: GridItemProps) => {
-  return (
-    <li className={`min-h-[14rem] list-none `}>
-      <div className="relative h-full rounded-2xl border p-2 md:rounded-3xl md:p-3">
-        <GlowingEffect
-          spread={40}
-          glow={true}
-          disabled={false}
-          proximity={64}
-          inactiveZone={0.01}
-        />
-        <div className="border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
-          <div className="relative flex flex-1 flex-col justify-between gap-3">
-            <div className="w-fit rounded-lg border border-gray-600 p-2">
-              {icon}
-            </div>
-            <div className="space-y-3">
-              <h3 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
-                {title}
-              </h3>
-              <h2 className="font-sans text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-white [&_b]:md:font-semibold [&_strong]:md:font-semibold">
-                {description}
-              </h2>
-            </div>
-          </div>
-        </div>
-      </div>
-    </li>
-  );
-};
